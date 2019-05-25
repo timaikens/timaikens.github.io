1d05340cbbb86aef75b671856fa17c02eea7b599
Tim Aikens <aikenst@gmail.com>
2019-05-25 15:28:38 -0400

    Even Github Repo Search: Setting homepage to relative path

---
+++ even-repo-search/precache-manifest.2f44a78e42dfa517c765dd05fc90b198.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8c9984f0c1d00419817e58db810ea97b",
+    "url": "./index.html"
+  },
+  {
+    "revision": "3afd0e81dcdba452aac6",
+    "url": "./static/css/main.eec36627.chunk.css"
+  },
+  {
+    "revision": "568285794ddb4b305a2e",
+    "url": "./static/js/2.3b7c1bc9.chunk.js"
+  },
+  {
+    "revision": "3afd0e81dcdba452aac6",
+    "url": "./static/js/main.c10f4e74.chunk.js"
+  },
+  {
+    "revision": "8c97409f0ee389fe75da",
+    "url": "./static/js/runtime~main.d653cc00.js"
+  },
+  {
+    "revision": "d2bb62d102e200d61bf69d0e2463629e",
+    "url": "./static/media/logo.d2bb62d1.png"
+  }
+]);
--- even-repo-search/service-worker.js
+++ even-repo-search/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.463dcd807fdbd891089badc022843108.js"
+  "./precache-manifest.2f44a78e42dfa517c765dd05fc90b198.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("./index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
