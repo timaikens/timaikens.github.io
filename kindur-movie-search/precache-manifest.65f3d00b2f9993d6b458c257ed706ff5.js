924352d30def33c758a045f2d4bc67345c085404
Tim Aikens <aikenst@gmail.com>
2019-06-04 00:12:46 -0400

    Kinder Movie Search: Initial commit of demo page

---
+++ kindur-movie-search/precache-manifest.65f3d00b2f9993d6b458c257ed706ff5.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "22454053b41750af6c7a896921f7ec5e",
+    "url": "./index.html"
+  },
+  {
+    "revision": "b590ee54b00f86ddbbf4",
+    "url": "./static/js/2.72528671.chunk.js"
+  },
+  {
+    "revision": "2f1225328170c344eaa9",
+    "url": "./static/js/main.e3d38155.chunk.js"
+  },
+  {
+    "revision": "8c97409f0ee389fe75da",
+    "url": "./static/js/runtime~main.d653cc00.js"
+  },
+  {
+    "revision": "051afb2fa8461b4c7e5ee05599bdba2b",
+    "url": "./static/media/logo.051afb2f.png"
+  },
+  {
+    "revision": "851eef80740773a0f41f3da735104e8f",
+    "url": "./static/media/screens.851eef80.svg"
+  }
+]);
