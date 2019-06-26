cc9f6c4d6e258d7a0e94f3c08ec3d3a93a8bb49b
Tim Aikens <aikenst@gmail.com>
2019-06-26 00:04:06 -0400

    Ant Racers: Initial commit of demo page

---
+++ sg-ant-racers/precache-manifest.4d71bbc6c5e70451e7c42816b1495011.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fc81ef66a20c91bb93c530b2445a3476",
+    "url": "./index.html"
+  },
+  {
+    "revision": "cd9ad95ea12bf58d702e",
+    "url": "./static/css/main.99d46eaa.chunk.css"
+  },
+  {
+    "revision": "e3e1726d43da044c5a6a",
+    "url": "./static/js/2.7cddf10a.chunk.js"
+  },
+  {
+    "revision": "cd9ad95ea12bf58d702e",
+    "url": "./static/js/main.76e43d83.chunk.js"
+  },
+  {
+    "revision": "8c97409f0ee389fe75da",
+    "url": "./static/js/runtime~main.d653cc00.js"
+  }
+]);
